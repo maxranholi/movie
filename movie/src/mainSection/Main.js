2fc0718c2c95d8f10991e317906116f6776e6ffc
Max-Ranholi <ranholimax@gmail.com>
2022-12-06 23:34:18 -0300

    requisição da api feita

--- movie/src/mainSection/Main.js
+++ movie/src/mainSection/Main.js
@@ -1,24 +1,35 @@
-import { ContainerMain } from "./Mainstyled";
+import { CardMovie, ContainerMain } from "./Mainstyled";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import { API_key, Imagepath } from "../constants/Url";
 
 const Main = () => {
-  const [title, setTitle] = useState("")
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     axios
-    .get(
-      "https://api.themoviedb.org/3/movie/76341?api_key=cd166892adfe5c00fbc2a247dd12001f"
-    )
-    .then((res) => {
-      setTitle(res.data.original_title);
-    }, []);
-  })
- 
-  return <ContainerMain>
-<h1>{title}</h1>
-  </ContainerMain>;
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_key}&language=en-US&page=1`
+      )
+      .then((res) => {
+        setMovies(res.data.results);
+        console.log(res.data.results);
+      });
+  }, []);
+  return (
+    <ContainerMain>
+      {movies.map((movie) => {
+        return (
+          <CardMovie key={movie.id}>
+            <img src={`${Imagepath}${movie.poster_path}`} />
+            <h2>{movie.title}</h2>
+            <h4>{movie.release_date}</h4>
+          </CardMovie>
+        );
+      })}
+    </ContainerMain>
+  );
 };
 
 export default Main;
--- movie/src/mainSection/Mainstyled.js
+++ movie/src/mainSection/Mainstyled.js
@@ -1,7 +1,54 @@
 import styled from "styled-components";
 
 export const ContainerMain = styled.div`
-height: 100vh;
-width: 100%;
-background-color: #E5E5E5;
+  height: 100%;
+  width: 100%;
+  background-color: #e5e5e5;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2rem 0rem;
+`;
+
+export const CardMovie = styled.div`
+
+img {
+    width: 176px;
+    height: 264px;
+    left: 112px;
+    top: 534px;
+    border: 1px solid #e7e7e7;
+    border-radius: 4px;
+  }
+
+  h2 {
+    width: 77px;
+    height: 24px;
+    left: 112px;
+    top: 806px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #000000;
+  }
+
+  h4 {
+    width: 84px;
+    height: 24px;
+    left: 320px;
+    top: 830px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #646464;
+  }
 `
